62b2047531328ca9e8850157ca7d3e1dfba5780a
moududulhaque <moududulhaque@gmail.com>
2024-03-25 11:56:51 +0600

    Navbar Edit

--- src/Pages/Shared/NavBar/NavBar2.jsx
+++ src/Pages/Shared/NavBar/NavBar2.jsx
@@ -7,59 +7,67 @@ import {
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import React from "react";
 
-
-
 // =================================================
 function NavList() {
-    return (
-      <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-        <Typography
-          as="li"
-          variant="small"
-          color="blue-gray"
-          className="p-1 font-medium"
+  return (
+    <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a
+          href="#"
+          className="flex items-center hover:text-blue-500 transition-colors"
         >
-          <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
-            Pages
-          </a>
-        </Typography>
-        <Typography
-          as="li"
-          variant="small"
-          color="blue-gray"
-          className="p-1 font-medium"
+          Pages
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a
+          href="#"
+          className="flex items-center hover:text-blue-500 transition-colors"
         >
-          <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
-            Account
-          </a>
-        </Typography>
-        <Typography
-          as="li"
-          variant="small"
-          color="blue-gray"
-          className="p-1 font-medium"
+          Account
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a
+          href="#"
+          className="flex items-center hover:text-blue-500 transition-colors"
         >
-          <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
-            Blocks
-          </a>
-        </Typography>
-        <Typography
-          as="li"
-          variant="small"
-          color="blue-gray"
-          className="p-1 font-medium"
+          Blocks
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a
+          href="#"
+          className="flex items-center hover:text-blue-500 transition-colors"
         >
-          <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
-            Docs
-          </a>
-        </Typography>
-      </ul>
-    );
-  }
+          Docs
+        </a>
+      </Typography>
+    </ul>
+  );
+}
 
 // ===================================================
-
-
 
 const NavBar2 = () => {
   const [openNav, setOpenNav] = React.useState(false);
@@ -75,16 +83,9 @@ const NavBar2 = () => {
     };
   }, []);
   return (
-    <Navbar className="mx-auto max-w-screen-xl px-6 py-3">
+    <Navbar className="mx-auto max-w-screen-xl px-6 py-3 shadow-none">
       <div className="flex items-center justify-between text-blue-gray-900">
-        <Typography
-          as="a"
-          href="#"
-          variant="h6"
-          className="mr-4 cursor-pointer py-1.5"
-        >
-          Material Tailwind
-        </Typography>
+        <img src="../../../../public/Images/Logo.png" alt="" />
         <div className="hidden lg:block">
           <NavList />
         </div>
